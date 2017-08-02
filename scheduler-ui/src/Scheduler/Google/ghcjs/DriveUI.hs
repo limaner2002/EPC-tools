fa34fb81df9e6e8bf94b1b93a33accfb0021f8ba
Joshua McCartney <josh@mcbk.local>
2017-08-02 11:40:23 -0400

    Added the GHCJS UI for the new scheduler.

---
+++ scheduler-ui/src/Scheduler/Google/ghcjs/DriveUI.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
+
+import Reflex.Dom
+import Reflex.Dom.Xhr
+import Scheduler.Google.Types
+import Lucid
+import Data.Aeson
+import Data.Text (Text, pack)
+import Data.Text.Lazy.Encoding (decodeUtf8, encodeUtf8)
+import Data.Text.Lazy (toStrict, fromStrict)
+import Control.Lens
+import Data.Semigroup
+import Control.Monad.Ref (Ref)
+import GHC.IORef (IORef)
+import Prelude
+
+main = mainWidgetWithHead Main.head runIt
+
+type DFile = DriveFile DriveFileType
+type DReq = DriveRequest DFile
+
+data BrowseEvent
+  = Crumb DFile
+  | Nav DFile
+  | Download DFile
+
+data NavType
+  = FileNav DFile
+  | CrumbNav DFile
+
+runIt :: MonadWidget t m => m ()
+runIt = do
+  pb <- getPostBuild
+  rec resDyn <- browseXhr $ leftmost [DriveRequest Root breadCrumbs <$ pb, evt']
+      let files = fmap (^. _Right . _1 . getFiles) resDyn
+          crumbs = fmap (^. _Right . _2 . bcCrumbs) resDyn
+      bcDyn <- el "div" $ simpleList crumbs (toDynHtml "div")
+      navDyn <- elAttr "table" ("class" =: "pure-table") $ do
+        el "thead" $ do
+          el "th" $ text "Name"
+          el "th" $ text "Type"
+        el "tbody" $ simpleList files (toDynHtml "tr")
+      let navEvt = switchPromptlyDyn $ fmap (fmap FileNav . leftmost) navDyn
+          bcEvt = switchPromptlyDyn $ fmap (fmap CrumbNav . leftmost) bcDyn
+          evt = leftmost [navEvt, bcEvt]
+          evt' = attachPromptlyDynWith mkReqEvt crumbs evt
+  return ()
+
+mkReqEvt :: [DFile] -> NavType -> DReq
+mkReqEvt crumbs (FileNav file) = DriveRequest file (bcCrumbs .~ crumbs $ breadCrumbs)
+mkReqEvt crumbs (CrumbNav file) = DriveRequest file (bcCrumbs .~ truncated $ breadCrumbs)
+  where
+    truncated = takeWhile (\f -> f ^. fId /= file ^. fId) crumbs
+
+browseReq :: DReq -> Maybe (XhrRequest Text)
+browseReq req
+  | typ == Just Folder || file == Root = Just $ xhrRequest "POST" "browse" cfg
+  | typ == Just Zip = Just $ xhrRequest "POST" "download" cfg
+  | otherwise = Nothing
+  where
+    cfg = (xhrRequestConfig_headers .~ ("Content-type" =: "application/json"))
+      . (xhrRequestConfig_sendData .~ (toStrict . decodeUtf8 $ encode req))
+      $ def
+    typ = file ^? fType
+    file = req ^. reqFile
+
+browseXhr :: MonadWidget t m => Event t DReq -> m (Dynamic t (Either String (Files, BreadCrumbs DFile)))
+browseXhr req = do
+  let fileReq = fmapMaybe browseReq req
+  evt <- performRequestAsync fileReq
+  holdDyn (Right (Fetching, breadCrumbs)) $ fmap (^. xhrResponse_responseText . to decodeResponse) evt
+
+decodeResponse :: Maybe Text -> Either String (Files, BreadCrumbs DFile)
+decodeResponse Nothing = Left "Received an empty response from the server."
+decodeResponse (Just t) = do
+  (fList, crumbs) <- eitherDecode . encodeUtf8 . fromStrict $ t
+  return $ (Files fList, crumbs)
+
+toNav :: MonadWidget t m => DFile -> m (Event t DFile)
+toNav file = do
+  (e, _) <- el' "div" $ text $ file ^. fName
+  let evt = domEvent Click e
+  return $ file <$ evt
+
+toDynHtml :: MonadWidget t m => Text -> Dynamic t DFile -> m (Event t DFile)
+toDynHtml elName d = do
+  let d' = fmap (toStrict . renderText . toHtml) d
+  e <- elDynHtml' elName d'
+  let evt = domEvent Click e
+
+  return $ attachPromptlyDynWith (\x _ -> x) d evt
+
+head :: MonadWidget t m => m ()
+head = do
+  elAttr "link" ( "rel" =: "stylesheet"
+               <> "href" =: "http://yui.yahooapis.com/pure/0.6.0/pure-min.css"
+                ) blank
